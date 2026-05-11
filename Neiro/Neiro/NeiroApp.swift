7e747288d1cccfbc5ca3f0caf811fe7e2f64ec5d
Ethan Shen-千早あのちゃん <EthanOLID@outlook.com>
2026-05-12 11:28:47 +1200

    updated files

---
+++ Neiro/Neiro/NeiroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeiroApp.swift
+//  Neiro
+//
+//  Created by Ethan Shen on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct NeiroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
